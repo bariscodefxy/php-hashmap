7d0f0f51e53db8fe147d93255285f52dca78f136
bariscodefx <bariscodefx@gmail.com>
2024-04-18 01:25:48 +0300

    LargeDataTest.php: downgrade data size to 500k

--- tests/LargeDataTest.php
+++ tests/LargeDataTest.php
@@ -9,14 +9,14 @@ final class LargeDataTest extends TestCase
         $hashmap = new \bariscodefx\PHPHashMap\HashMap();
 
         $time = microtime(true);
-        for($i = 0; $i < 1000000; $i++)
+        for($i = 0; $i < 500000; $i++)
         {
             $hashmap->set('key.' . $i, 'data_' . $i);
         }
         fwrite(STDOUT,  __FUNCTION__ . "(): Writing it finished in " . microtime(true) - $time . ".\n");
 
         $time = microtime(true);
-        $hashmap->get('key.1000000');
+        $hashmap->get('key.500000');
         fwrite(STDOUT,  __FUNCTION__ . "(): Reading it finished in " . microtime(true) - $time . ".\n");
 
         $this->assertTrue(true);
